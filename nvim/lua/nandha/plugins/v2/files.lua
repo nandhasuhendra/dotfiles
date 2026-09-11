3a665c46f3bfb872e6b9c53490989b35cbf5c471
Nanda Suhendra <nandhasuhendra@gmail.com>
2026-09-12 00:48:27 +0700

    feat(nvim): replace Oil with Neo-tree

--- nvim/lua/nandha/plugins/v2/files.lua
+++ nvim/lua/nandha/plugins/v2/files.lua
@@ -1,13 +1,40 @@
 return {
   {
-    "stevearc/oil.nvim",
-    cmd = "Oil",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
     opts = {
-      default_file_explorer = true,
-      delete_to_trash = true,
-      view_options = { show_hidden = true },
-      columns = { "icon", "permissions", "size", "mtime" },
+      close_if_last_window = true,
+
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
+
+        hijack_netrw_behavior = "open_current",
+
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+
+      window = {
+        width = 30,
+      },
+
+      default_component_configs = {
+        indent = {
+          with_expanders = true,
+        },
+      },
     },
   },
 }
