4120081c5bb1c7a2cb5da9915dde656fe814960a
Nanda Suhendra <nandhasuhendra@gmail.com>
2026-09-11 20:42:10 +0700

    feat(nvim): add neotest

---
+++ nvim/lua/nandha/plugins/v2/testing.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/nvim-nio",
+      "nvim-neotest/neotest-rspec",
+      "nvim-neotest/neotest-jest",
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-go",
+    },
+    keys = { "<leader>tt", "<leader>tf", "<leader>to" },
+    opts = function()
+      return {
+        adapters = {
+          require("neotest-rspec"),
+          require("neotest-jest"),
+          require("neotest-python"),
+          require("neotest-go"),
+        },
+      }
+    end,
+  },
+}
