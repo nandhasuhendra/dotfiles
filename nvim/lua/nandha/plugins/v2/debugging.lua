fd595a711b71a93ec070fe2e1d8b6fb3f65da804
Nanda Suhendra <nandhasuhendra@gmail.com>
2026-09-11 20:42:06 +0700

    fix(nvim): enable dap adapter handlers

--- nvim/lua/nandha/plugins/v2/debugging.lua
+++ nvim/lua/nandha/plugins/v2/debugging.lua
@@ -12,9 +12,8 @@ return {
     "jay-babu/mason-nvim-dap.nvim",
     dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
     opts = {
-      ensure_installed = { "delve", "debugpy", "js", "ruby" },
+      ensure_installed = { "delve", "debugpy", "js" },
       automatic_installation = true,
-      handlers = {},
     },
   },
 }
