b802f8dc25cd322a21faa2972b1843fc213b0a0d
nandha-suhendra <nandhasuhendra@gmail.com>
2026-09-14 21:46:45 +0700

    resize the file explorer

--- nvim/lua/nandha/plugins/v2/files.lua
+++ nvim/lua/nandha/plugins/v2/files.lua
@@ -1,40 +1,40 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    cmd = "Neotree",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      close_if_last_window = true,
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		cmd = "Neotree",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			close_if_last_window = true,
 
-      filesystem = {
-        follow_current_file = {
-          enabled = true,
-          leave_dirs_open = false,
-        },
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
 
-        hijack_netrw_behavior = "open_current",
+				hijack_netrw_behavior = "open_current",
 
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-      },
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
 
-      window = {
-        width = 30,
-      },
+			window = {
+				width = 45,
+			},
 
-      default_component_configs = {
-        indent = {
-          with_expanders = true,
-        },
-      },
-    },
-  },
+			default_component_configs = {
+				indent = {
+					with_expanders = true,
+				},
+			},
+		},
+	},
 }
